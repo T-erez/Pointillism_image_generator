d8a90b4f15b2f3adbe48293583d0632d15964f8c
Tereza Ferenčíková <97316999@cuni.cz>
2022-09-03 13:52:09 +0200

    Add class Pattern

---
+++ Pointillism.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pointillism_image_generator
+{
+    public struct Pattern
+    {
+        public int xIndex;
+        public int yIndex;
+        public int colorARGB;
+        public int angle;
+        public Pattern(int xIndex, int yIndex, int color, int angle)
+        {
+            this.xIndex = xIndex;
+            this.yIndex = yIndex;
+            this.colorARGB = color;
+            this.angle = angle;
+        }
+    }
+}
